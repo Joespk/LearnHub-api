5d6b30a0a23a7e61aef2c6612d3cd341601298f0
Joespk <supakritjoe@gmail.com>
2023-11-01 14:55:41 +0700

    dto

---
+++ src/dto/user.ts
@@ -0,0 +1,12 @@
+export interface IUserDto {
+  id: string;
+  username: string;
+  name: string;
+  registerdAt: string;
+}
+
+export interface ICredentialDto {
+  name: string;
+  username: string;
+  password: string;
+}
